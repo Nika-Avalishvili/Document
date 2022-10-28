fc6e99381e2f0c2716b04f141b2ccdc3cc119954
Nika Avalishvili <nika.avalishvili@solveva.com>
2022-10-28 10:32:16 +0400

    Create Document application

---
+++ src/test/java/com/example/document/DocumentApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example.document;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+class DocumentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
